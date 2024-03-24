cd3a479e05eee4884d5f525629c4fd0147981cdf
Nile-Waldal <55334108+Nile-Waldal@users.noreply.github.com>
2024-03-24 11:25:23 -0700

    Add files via upload

--- Capstone GUI.py
+++ Capstone GUI.py
@@ -11,6 +11,7 @@ import csv
 import parse
 import numpy as np
 import struct
+import os
  
 class GUI(Frame):
     def __init__(self, master):
@@ -21,7 +22,7 @@ class GUI(Frame):
         self.grid(padx=50,pady=25)
         self.createwidgets()
     def createwidgets(self):
-        self.title=Label(self,text="Dynamic Phantom GUI")
+        self.title=Label(self,text="Dynamic Phantom GUI", font=("Arial",30))
         self.side=Sidebar(self)
         self.plot=Plot(self)
         self.title.grid(row=0,column=0, columnspan=5)
@@ -31,32 +32,80 @@ class Plot(Frame):
     def __init__(self,master):
         Frame.__init__(self,master)
         fig=Figure(figsize=(5,5),dpi=100)
-        ax=fig.add_subplot(111)
-        line,=ax.plot([],[],lw=2)
+        axSph=fig.add_subplot(221)
+        axSpher=fig.add_subplot(222)
+        axKid=fig.add_subplot(223)
+        axKider=fig.add_subplot(224)
+        lineSph,=axSph.plot([],[],lw=2)
+        lineSpher,=axSpher.plot([],[],lw=2)
+        lineKid,=axKid.plot([],[],lw=2)
+        lineKider,=axKider.plot([],[],lw=2)
         canvas=FigureCanvasTkAgg(fig,master=self)
         
         canvas.get_tk_widget().pack()
         toolbar = NavigationToolbar2Tk(canvas,self)
         max_points = 50
         # fill initial artist with nans (so nothing draws)
-        line, = ax.plot(np.arange(max_points), 
+        lineSph, = axSph.plot(np.arange(max_points), 
                         np.ones(max_points, dtype=float)*np.nan, 
                         lw=2)
-        global anim
-        anim = animation.FuncAnimation(fig, (lambda i, port=(arduino if COMconnected else ""):self.animate(port,i)),  frames=200, interval=20,        blit=False)
+        lineSpher, = axSpher.plot(np.arange(max_points), 
+                        np.ones(max_points, dtype=float)*np.nan, 
+                        lw=2)
+        lineKid, = axKid.plot(np.arange(max_points), 
+                        np.ones(max_points, dtype=float)*np.nan, 
+                        lw=2)
+        lineKider, = axKider.plot(np.arange(max_points), 
+                        np.ones(max_points, dtype=float)*np.nan, 
+                        lw=2)
+        global anim1,anim2,anim3,anim4
+        anim1 = animation.FuncAnimation(fig, (lambda i, port=(arduino if COMconnected else ""):self.animateSph(port,i)),  frames=200, interval=20,blit=False)
+        anim2 = animation.FuncAnimation(fig, (lambda i, port=(arduino if COMconnected else ""):self.animateSpher(port,i)),  frames=200, interval=20,blit=False)
+        anim3 = animation.FuncAnimation(fig, (lambda i, port=(arduino if COMconnected else ""):self.animateKid(port,i)),  frames=200, interval=20,blit=False)
+        anim4 = animation.FuncAnimation(fig, (lambda i, port=(arduino if COMconnected else ""):self.animateKider(port,i)),  frames=200, interval=20,blit=False)
 
         plt.show()
 
-    def animate(self,port,i):
+    def animateSph(self,port,i):
         if port=="":
-            line.set_ydata(0)
-            return line,
+            lineSph.set_ydata(0)
+            return lineSph,
         else:
             y = port.readline()  # I assume this 
-            old_y = line.get_ydata()  # grab current data
+            old_y = lineSph.get_ydata()  # grab current data
             new_y = np.r_[old_y[1:], y]  # stick new data on end of old data
-            line.set_ydata(new_y)        # set the new ydata
-            return line,
+            lineSph.set_ydata(new_y)        # set the new ydata
+            return lineSph,
+    def animateSpher(self,port,i):
+        if port=="":
+            lineSpher.set_ydata(0)
+            return lineSpher,
+        else:
+            y = port.readline()  # I assume this 
+            old_y = lineSpher.get_ydata()  # grab current data
+            new_y = np.r_[old_y[1:], y]  # stick new data on end of old data
+            lineSpher.set_ydata(new_y)        # set the new ydata
+            return lineSpher,
+    def animateKid(self,port,i):
+        if port=="":
+            lineKid.set_ydata(0)
+            return lineKid,
+        else:
+            y = port.readline()  # I assume this 
+            old_y = lineKid.get_ydata()  # grab current data
+            new_y = np.r_[old_y[1:], y]  # stick new data on end of old data
+            lineKid.set_ydata(new_y)        # set the new ydata
+            return lineKid,
+    def animateKider(self,port,i):
+        if port=="":
+            lineKider.set_ydata(0)
+            return lineKider,
+        else:
+            y = port.readline()  # I assume this 
+            old_y = lineKider.get_ydata()  # grab current data
+            new_y = np.r_[old_y[1:], y]  # stick new data on end of old data
+            lineKider.set_ydata(new_y)        # set the new ydata
+            return lineKider,
         
 class Sidebar(Frame):
     def __init__(self,master):
@@ -87,21 +136,21 @@ class Sidebar(Frame):
         self.entLab=Label(self,text="Decay Constant")
         self.ent=Entry(self,textvariable=dec)
 
-        self.COM.grid(row=0,column=0)
-        self.COMlist.grid(row=0,column=1)
-        self.buttonCOM.grid(row=0,column=2)
-        self.selFileSphere.grid(row=1,column=0)
-        self.buttonOpenSphere.grid(row=1,column=1)
-        self.buttonLoadCurveSphere.grid(row=1,column=2)
-        self.selFileKidney.grid(row=2,column=0)
-        self.buttonOpenKidney.grid(row=2,column=1)
-        self.buttonLoadCurveKidney.grid(row=2,column=2)
-        self.buttonCali.grid(row=3,column=0)
-        self.buttonFlush.grid(row=3,column=1)
-        self.buttonRun.grid(row=4,column=0)
-        self.buttonStop.grid(row=4,column=1)
-        self.buttonPause.grid(row=4,column=2)
-        self.grid(padx=50,pady=25)
+        self.COM.grid(row=0,column=0,padx=5,pady=5)
+        self.COMlist.grid(row=0,column=1,padx=5,pady=5)
+        self.buttonCOM.grid(row=0,column=2,padx=5,pady=5)
+        self.selFileSphere.grid(row=1,column=0,padx=5,pady=5)
+        self.buttonOpenSphere.grid(row=1,column=1,padx=5,pady=5)
+        self.buttonLoadCurveSphere.grid(row=1,column=2,padx=5,pady=5)
+        self.selFileKidney.grid(row=2,column=0,padx=5,pady=5)
+        self.buttonOpenKidney.grid(row=2,column=1,padx=5,pady=5)
+        self.buttonLoadCurveKidney.grid(row=2,column=2,padx=5,pady=5)
+        self.buttonCali.grid(row=3,column=1,padx=5,pady=5)
+        self.buttonRun.grid(row=4,column=0,padx=5,pady=5)
+        self.buttonStop.grid(row=4,column=1,padx=5,pady=5)
+        self.buttonPause.grid(row=4,column=2,padx=5,pady=5)
+        self.buttonFlush.grid(row=5,column=1,padx=5,pady=5)
+        self.grid(padx=25,pady=25)
         
 def startTest(pauBut,stopBut,port):
     port.write((1).to_bytes(1,"big"))
@@ -133,7 +182,7 @@ def stopTest(port):
 def openFile(selected_file_label,loadBut):
     file_path = filedialog.askopenfilename(title="Select a File", filetypes=[("CSV", "*.csv")])
     if file_path:
-        selected_file_label.config(text=f"Selected File: {file_path}")
+        selected_file_label.config(text=f"Selected File: {os.path.basename(file_path)}")
         process_file(file_path,selected_file_label,loadBut)
 def process_file(file_path,selected_file_label,loadBut):
     # Implement your file processing logic here
