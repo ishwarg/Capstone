e7d1216649d20420207e14e48a64b1ecb44fd9e7
Ishwarjot Grewal <ishwarjotgrewal@hotmail.com>
2024-04-01 17:33:09 -0700

    Almost done version of Fluid Controller. Need to implement dynamic gain scheduling

--- Fluid_Controller/Fluid_Controller.ino
+++ Fluid_Controller/Fluid_Controller.ino
@@ -1,8 +1,8 @@
 #include <PID_v1.h>
 
-#define WATER 5
-#define SALT 6
-#define CONCENTRATION 0
+#define WATER 3
+#define SALT 8
+#define CONCENTRATION 1
 
 #define RUN 1
 #define CALIBRATE 2
@@ -17,12 +17,29 @@
 
 #define FULL_SPEED 255
 #define OFF 0
-#define WATER_SPEED 65
+#define WATER_SPEED 190
+#define SALT_SPEED 120
+#define WATER_MIN 50
+#define SALT_MIN 75
+
+#define SETPOINT_THRESHOLD 40
+#define WINDOW 5
+
+#define SALTKP 0.09
+#define SALTKI 0.18
+#define SALTKD 0.04
+#define WATERKP 0.0
+#define WATERKI 0.0
+#define WATERKD 0.0
+
+
+
 
 double Setpoint, Input, Output;
 
-double Kp = 2.3, Ki = 1.5, Kd = 0;
+double Kp = SALTKP, Ki = SALTKI, Kd = SALTKD;
 PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+
 
 int lowSpeed = 0;
 int highSpeed = 0;
@@ -37,111 +54,123 @@ float conc = 0;
 float salt_conc = 0;
 float water_conc = 0;
 
-int begin;
 int runStart;
+int flushStart;
+unsigned long begin=0;
+int elapsedTime;
 int state = 5;
+int pidState = 0;
 bool flowState = false;
 int numRows = 0;
-double curve[MAX_ROWS][2]  ={ { 0.0, 50.0 }, { 2.0, 40.0 }, { 4.0, 30.0 }, { 8.0, 20.0 } };
+double curve[MAX_ROWS][2] = {{0.0, 20.0}, {60.0, 0.0}};
 int curveIndex = 0;
 
+const byte numRowsSize = sizeof(int);
+const byte dataPointSize = sizeof(double);
+const int BUFFER_SIZE = 5; // 4 bytes for the string + 1 for null terminator
+char buffer[BUFFER_SIZE]; // Buffer to store the received bytes
+
 void setup() {
-  
-  Input = analogRead(CONCENTRATION);
-  Setpoint = 20;  // initial target concentration; will be changed when the calibration curve is loaded in
-  numRows = 4;    // TEMPORARY LINE
-
   pinMode(WATER, OUTPUT);
-  digitalWrite(WATER, LOW);
-
   pinMode(SALT, OUTPUT);
-  digitalWrite(SALT, LOW);
+  Input = analogRead(CONCENTRATION);
+  numRows = 2;    // TEMPORARY LINE
 
-  Serial.begin(9600);
+
+  Serial.begin(115200);
+
   myPID.SetMode(MANUAL);
+  myPID.SetSampleTime(8);
+  myPID.SetControllerDirection(DIRECT);
+  myPID.SetOutputLimits(SALT_MIN, 255);
 
-  analogWrite(WATER, WATER_SPEED);
+  
   state = STOP;      // change this to be in the Stopped State once GUI is fully done
-  begin = millis();  // beginning the timer for when the code starts
-  Serial.write(45);
+  
+  
 }
 
 void loop() {
 
 
   for (int i = 0; i < 50; i++) {
-    conc += analogRead(0);
+    conc += analogRead(CONCENTRATION);
   }
   conc = conc / 50;
-
-
-  /*  FLOW METER CODE
-    if (newSig-prevSig>100 && !flowstate){
-      time = micros();
-      flowstate = true;
-    }
-
-    if (newSig-prevSig<100 && flowstate){
-      time = micros()-time;
-      time = time/1000000;
-
-      flowstate = false;
-      freq = 1.0/time/2.0;
-
-      flow = freq/38;
-    }
-    */
-
+    
 
   if (state == RUN) {
+    
+    
+    if (millis() - runStart > (int)curve[curveIndex + 1][0] * 1000){
+    curveIndex++;
+    
 
-    if (millis() - runStart < RAMP_TIME) {
-      // Setpoint = 20.0 * exp(-1 * double(millis() - begin) / 100000.0); // REPLACE WITH ACTUAL INPUT CURVE CODE
-      Setpoint = curve[0][1];
-
-    } else {
-      if (millis() - runStart - RAMP_TIME > (double)curve[curveIndex + 1][0] * 1000)
-        curveIndex++;
-      Setpoint = curve[curveIndex][1];
     }
+    
+    Setpoint = curve[curveIndex][1];
+  
 
     if (curveIndex >= numRows) {
       state = STOP;  // MAYBE HAVE A MESSAGE THAT PRINTS OUT THE PROFILE HAS BEEN COMPLETED
       Serial.println("Finished... Stopping...");
       curveIndex = 0;
-      
     }
+
     Input = conc;
-    myPID.Compute();  // change code so that there is saturation from the PID object itself
-    if (Output > 255)
-      Output = 255;
-    if (Output < 0) {
-      Output = 0;
-    }
-
-    // Serial.println((int)(conc-Setpoint)/Setpoint*100);
-
+    myPID.SetSampleTime(8);
+    
+    myPID.Compute();
+    //Output  = map_range(Output, 0, 255, SALT_MIN, 255);
+    int waterCommand = linearInterpolation(Setpoint,180,20,70,240);
+    
+    analogWrite(WATER, waterCommand);
     analogWrite(SALT, (int)Output);
+
+    Serial.print(0);
+    Serial.print(" ");
+    Serial.print(Setpoint);
+    Serial.print(" ");
+    Serial.print(conc);
+    Serial.print(" ");
+    Serial.print(waterCommand);
+    Serial.print(" ");
+    Serial.print((int)Output);
+    Serial.print(" ");
+    Serial.print(Setpoint*1.1);
+    Serial.print(" ");
+    Serial.println(Setpoint*0.9);
+    
+ 
+    
+
 
     // Serial.print(0);
     // Serial.print(" ");
     // Serial.print(25);
     // Serial.print(" ");
-    Serial.println(Setpoint);
-    // Serial.print(" ");
-    // Serial.println((int)(conc));
+    
+    //delay(5);
+    
+    
   }
   if (state == FLUSH) {
     Serial.println("flush");
-    int flushStart = millis();
+   
 
     analogWrite(WATER, FULL_SPEED);
     analogWrite(SALT, FULL_SPEED);
+    
 
     while (millis() - flushStart <= 10000) {
+      Serial.print("flushing");
+      Serial.print(" ");
+      Serial.println(analogRead(CONCENTRATION));
+      
     }
     analogWrite(WATER, OFF);
     analogWrite(SALT, OFF);
+    Serial.println("Done Flush");
     state = STOP;
   }
   if (state == CALIBRATE){
@@ -188,18 +217,35 @@ void loop() {
     while (!Serial.available()) {
     }
     if (Serial.available() > 0) {
-      numRows = Serial.read();
-      Serial.println(numRows);
+      // Serial.readBytes((char*)&numRows, numRowsSize); // Read numRows as an int
+      String receivedString = Serial.readStringUntil('\0');
+      Serial.println("Received numRows: " + receivedString);
+      numRows = receivedString.toInt();
 
       // Define the 2D array to store received data
+
+    
+
+      // Convert bytes back into 2D list of doubles
       for (int i = 0; i < numRows; i++) {
-        for (int j = 0; j < 2; j++) {  // Assuming 2 columns
-          // Deserialize the received bytes into a double
-          double value;
-          Serial.readBytes((char *)&value, sizeof(double));
-          curve[i][j] = value;
+        for (int j = 0; j < 2; j++) {
+          
+          receivedString = Serial.readStringUntil('\0');
+          Serial.println(receivedString);
+          //Serial.println(receivedString);
+          curve[i][j] = receivedString.toFloat();
+          //Serial.println(curve[i][j]);
         }
-      }
+    }
+      Serial.println("Finished Loading Curve");
+      for (int i = 0; i < numRows; i++) {
+      Serial.print("Row ");
+      Serial.print(i);
+      Serial.print(": ");
+      Serial.print(curve[i][0]);
+      Serial.print(", ");
+      Serial.println(curve[i][1]);
+    }
     }
 
     state = STOP;
@@ -216,30 +262,102 @@ void loop() {
 
   if (Serial.available() > 0) {
     
+    
     // read incoming serial data:
 
-    char inChar = Serial.read();
-    if (inChar == '1') {
+    String inChar = Serial.readStringUntil('\n');
+    Serial.println(inChar);
+    if (inChar.equals("1")) {
       state = RUN;
-      Serial.println(RUN);
+      Serial.println("About to start...");
       myPID.SetMode(AUTOMATIC);
       runStart = millis();
-    } else if (inChar == '2') {
+      curveIndex = 0;
+      analogWrite(WATER, WATER_SPEED);
+    } else if (inChar.equals("2")) {
       Serial.println(CALIBRATE);
       state = CALIBRATE;
-    } else if (inChar == '3') {
-      Serial.println(FLUSH);
+    } else if (inChar.equals("3")) {
       state = FLUSH;
-    } else if (inChar == '4') {
+      flushStart = millis();
+    } else if (inChar.equals("4")) {
       Serial.println(CURVELOAD);
       state = CURVELOAD;
-    } else if (inChar == '5') {
+    } else if (inChar.equals("5")) {
       Serial.println("Stopping");
       state = STOP;
     }
-    // else {
-    //   Serial.print("Invalid Command... Stopping");
-    //   state = STOP;
-    // }
+     
+    else if (inChar.equals("6")) {
+      Serial.println(myPID.GetKp(),3);
+      Serial.println(myPID.GetKi(),3);
+      Serial.println(myPID.GetKd(),3);   
+    }
+
+    else if (inChar.equals("7")){
+        Serial.println("Changing Gains");
+        String receivedString;
+        double tempKp, tempKi, tempKd;
+        while (!Serial.available()) {
+      }
+      if (Serial.available() > 0) {
+          receivedString = Serial.readStringUntil('\n');
+          Serial.println(receivedString);
+          tempKp = receivedString.toDouble();
+      }
+      while (!Serial.available()) {
+      }
+      if (Serial.available() > 0) {
+          receivedString = Serial.readStringUntil('\n');
+          Serial.println(receivedString);
+          tempKi = receivedString.toDouble();
+      }
+      while (!Serial.available()) {
+      }
+      if (Serial.available() > 0) {
+          receivedString = Serial.readStringUntil('\n');
+          Serial.println(receivedString);
+          tempKd = receivedString.toDouble();
+      }
+      Kp = tempKp;
+      Ki = tempKi;
+      Kd = tempKd;
+      myPID.SetTunings(Kp,Ki,Kd);
+      
+      Serial.println("Done Changing Gains");
+    }
+
+    else if (inChar.equals("8")){
+      Serial.println("Changing Setpoint...");
+        String receivedString;
+        double tempSet;
+        while (!Serial.available()) {
+        }
+      if (Serial.available() > 0) {
+          receivedString = Serial.readString();
+          Serial.println(receivedString);
+          tempSet = receivedString.toDouble();
+      }
+      curve[0][1]= tempSet;
+      Serial.println(curve[0][1]); 
+      Serial.println("Done Changing Setpoint..");
+
+    }
+    
   }
+}
+
+int map_range(float value, float old_min, float old_max, float new_min, float new_max) {
+    // Scale the value from the old range to a value in the range [0, 1]
+    float normalized_value = (value - old_min) / (old_max - old_min);
+    // Map this normalized value to the new range
+    int new_value = new_min + normalized_value * (new_max - new_min);
+    return new_value;
+}
+
+int linearInterpolation( float x, float x1, float x0, float y1, float y0){
+  float normalized_Value = (y1-y0)/(x1-x0)*(x-x0)+y0;
+  return (int)normalized_Value;
+
+
 }
